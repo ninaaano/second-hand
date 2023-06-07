091624b99da4f585601d276460278c4b498a7d68
Life is a ninanino <tvxqinak@naver.com>
2023-06-07 16:41:53 +0900

    test: jsx 파일 수정
    빌드시 오류가 발생하여 파일 내용 변경

--- test/src/components/common/DropDown/DropDownMenu/index.jsx
+++ test/src/components/common/DropDown/DropDownMenu/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { useFilterDispatchContext, useFilterStateContext } from '../../../../context/filterContext';
@@ -8,36 +8,42 @@ import Icon from '../../Icon';
 import { $MenuWrapper, $MenuLeftWrapper, $MenuImg, $MenuText, $LabelColor } from './style';
 
 const DropDownMenu = ({
-  menuId,
-  menuType,
-  menuImg = null,
-  menuText,
-  backgroundColor,
-  dropDownType,
-  onSelectItem,
-  isSelectItem,
-}) => {
-  if (dropDownType !== 'sideBar') {
-    const filterDispatch = useFilterDispatchContext();
-    const filterState = useFilterStateContext();
-    const checkedOption = filterState[menuType];
+                        menuId,
+                        menuType,
+                        menuImg = null,
+                        menuText,
+                        backgroundColor,
+                        dropDownType,
+                        onSelectItem,
+                        isSelectItem,
+                      }) => {
+  const filterDispatch = useFilterDispatchContext();
+  const filterState = useFilterStateContext();
+  const checkedOption = filterState[menuType];
 
-    const isChecked = checkedOption === menuId;
-    const menuClickHandler = () => {
-      filterDispatch({ type: FILTER_ACTION_TYPES.CLICK_MENU, payload: { filterType: menuType, id: menuId } });
-    };
+  useEffect(() => {
+    if (dropDownType !== 'sideBar') {
+      const isChecked = checkedOption === menuId;
+      const menuClickHandler = () => {
+        filterDispatch({ type: FILTER_ACTION_TYPES.CLICK_MENU, payload: { filterType: menuType, id: menuId } });
+      };
 
-    return (
-      <$MenuWrapper type="button" onClick={dropDownType !== 'sideBar' ? menuClickHandler : () => {}}>
-        <$MenuLeftWrapper>
-          {backgroundColor && <$LabelColor backgroundColor={backgroundColor} />}
-          {menuImg !== null && <$MenuImg src={menuImg} />}
-          <$MenuText $isChecked={isChecked}>{menuText}</$MenuText>
-        </$MenuLeftWrapper>
-        <Icon name={isChecked ? 'checkOnCircle' : 'checkOffCircle'} />
-      </$MenuWrapper>
-    );
-  }
+      const handleClick = () => {
+        if (dropDownType !== 'sideBar') {
+          menuClickHandler();
+        }
+      };
+
+      return () => {
+        // Cleanup 함수 등록
+        if (dropDownType !== 'sideBar') {
+          filterDispatch({ type: FILTER_ACTION_TYPES.CLICK_MENU, payload: { filterType: menuType, id: null } });
+        }
+      };
+
+      // 배열에 의존성 목록 추가
+    }
+  }, [dropDownType, menuId, menuType, filterDispatch, checkedOption]);
 
   const selectItemHandler = () => {
     onSelectItem((prev) => {
@@ -51,19 +57,19 @@ const DropDownMenu = ({
   };
 
   return (
-    <$MenuWrapper type="button" onClick={selectItemHandler}>
-      <$MenuLeftWrapper>
-        {backgroundColor && <$LabelColor backgroundColor={backgroundColor} />}
-        {menuImg !== null && <$MenuImg src={menuImg} />}
-        <$MenuText $isChecked={isSelectItem === menuId}>{menuText}</$MenuText>
-      </$MenuLeftWrapper>
-      <Icon name={isSelectItem === menuId ? 'checkOnCircle' : 'checkOffCircle'} />
-    </$MenuWrapper>
+      <$MenuWrapper type="button" onClick={dropDownType !== 'sideBar' ? selectItemHandler : undefined}>
+        <$MenuLeftWrapper>
+          {backgroundColor && <$LabelColor backgroundColor={backgroundColor} />}
+          {menuImg !== null && <$MenuImg src={menuImg} />}
+          <$MenuText $isChecked={isSelectItem === menuId}>{menuText}</$MenuText>
+        </$MenuLeftWrapper>
+        <Icon name={isSelectItem === menuId ? 'checkOnCircle' : 'checkOffCircle'} />
+      </$MenuWrapper>
   );
 };
 
 DropDownMenu.propTypes = {
-  menuId: PropTypes.oneOfType(['string', 'number']),
+  menuId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   menuType: PropTypes.string.isRequired,
   menuImg: PropTypes.string,
   menuText: PropTypes.string.isRequired,
