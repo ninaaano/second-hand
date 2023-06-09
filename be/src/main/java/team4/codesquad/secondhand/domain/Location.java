4b8c88368606a9d0ce84489ab3f8ce4064c74ec8
Hyun <ghkdgus29@naver.com>
2023-06-09 10:48:22 +0900

    feat #5: 로케이션 엔티티 생성

---
+++ be/src/main/java/team4/codesquad/secondhand/domain/Location.java
@@ -0,0 +1,23 @@
+package team4.codesquad.secondhand.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@ToString
+public class Location {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer locationId;
+
+    private String district;
+    private String city;
+    private String town;
+}
---
+++ be/src/test/java/team4/codesquad/secondhand/repository/LocationRepositoryTest.java
@@ -0,0 +1,23 @@
+package team4.codesquad.secondhand.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import team4.codesquad.secondhand.domain.Location;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LocationRepositoryTest {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Test
+    void init() {
+        List<Location> locations = locationRepository.findAll();
+        System.out.println(locations.get(0));
+    }
+}
