ec8d87850218e291e267ae52eac78f2e086286ee
Life is a ninanino <tvxqinak@naver.com>
2023-06-12 16:03:40 +0900

    feat: 카테고리 목록 전체 조회 기능 구현

--- be/src/main/java/team4/codesquad/secondhand/constant/ResponseMessage.java
+++ be/src/main/java/team4/codesquad/secondhand/constant/ResponseMessage.java
@@ -3,4 +3,5 @@ package team4.codesquad.secondhand.constant;
 public abstract class ResponseMessage {
 
     public static final String READ_PRODUCT = "상품 정보 조회 성공";
+    public static final String READ_CATEGORY = "카테고리 정보 조회 성공";
 }
---
+++ be/src/main/java/team4/codesquad/secondhand/service/CategoryService.java
@@ -0,0 +1,31 @@
+package team4.codesquad.secondhand.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team4.codesquad.secondhand.domain.Category;
+import team4.codesquad.secondhand.domain.dto.CategoryDTO;
+import team4.codesquad.secondhand.domain.dto.CategoryListDTO;
+import team4.codesquad.secondhand.repository.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+
+    // 카테고리 목록
+    public CategoryListDTO findAll(){
+        List<Category> category = categoryRepository.findAll();
+        return new CategoryListDTO(category.stream()
+                .map(CategoryDTO::new)
+                .collect(Collectors.toList()));
+    }
+
+}
