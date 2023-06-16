1074a6b0dbd61cae213f16aca7f5da95fdba2944
Life is a ninanino <tvxqinak@naver.com>
2023-06-16 14:39:20 +0900

    feat: 작성화면생성시 필요한 정보 DTO

---
+++ be/src/main/java/team4/codesquad/secondhand/domain/dto/ProductFormDTO.java
@@ -0,0 +1,12 @@
+package team4.codesquad.secondhand.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductFormDTO {
+
+    private CategoryListDTO randomCategoryList;
+
+}
