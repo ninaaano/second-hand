ab71e56a1f7d3dfb2a2fd9a278d02c7ec49674f9
Life is a ninanino <tvxqinak@naver.com>
2023-06-20 09:45:41 +0900

    feat: Controller 생성 추가

--- be/src/main/java/team4/codesquad/secondhand/controller/ProductController.java
+++ be/src/main/java/team4/codesquad/secondhand/controller/ProductController.java
@@ -4,12 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team4.codesquad.secondhand.constant.ResponseMessage;
 import team4.codesquad.secondhand.controller.dto.Message;
 import team4.codesquad.secondhand.service.ProductService;
+import team4.codesquad.secondhand.service.dto.ProductRequestDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +26,12 @@ public class ProductController {
     public ResponseEntity<Message> product(@PathVariable Integer productId) {
         Message message = new Message(HttpStatus.OK, ResponseMessage.READ_PRODUCT_DETAIL, productService.increaseViewsAndRetrieveProduct(productId));
         return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    // TODO : 상품 판매
+    @PostMapping("api/product")
+    public ResponseEntity<Message> save(@RequestBody ProductRequestDTO request){
+        Message message = new Message(HttpStatus.OK, ResponseMessage.CREATE_OK, productService.createProduct(request));
+        return new ResponseEntity<>(message,HttpStatus.OK);
     }
 }
