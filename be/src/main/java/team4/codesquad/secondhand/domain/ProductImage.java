714750fb059ae56d35161f91c31d74df04a3c3e8
Life is a ninanino <tvxqinak@naver.com>
2023-06-21 21:56:12 +0900

    feat: 상품 생성시 이미지 추가하는 빌더패턴 추가

--- be/src/main/java/team4/codesquad/secondhand/domain/ProductImage.java
+++ be/src/main/java/team4/codesquad/secondhand/domain/ProductImage.java
@@ -1,13 +1,16 @@
 package team4.codesquad.secondhand.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class ProductImage {
 
@@ -21,5 +24,11 @@ public class ProductImage {
     @JoinColumn(name = "product_id")
     @JsonIgnore
     private Product product;
+
+    @Builder
+    public ProductImage(String imageUrl, Product product) {
+        this.imageUrl = imageUrl;
+        this.product = product;
+    }
 
 }
