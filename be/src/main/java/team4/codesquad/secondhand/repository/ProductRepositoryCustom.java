0a7128c604042fb0b6fc0c6d9ea469ad44e43428
Hyun <ghkdgus29@naver.com>
2023-06-21 18:57:16 +0900

    feat #68: 상품 필터링 기능 구현
    - locationId = 지역 정보를 바탕으로 필터링
    - categoryId = 카테고리 정보를 바탕으로 필터링
    - saleStatus = 상품 상태 정보를 바탕으로 필터링

--- be/src/main/java/team4/codesquad/secondhand/repository/ProductRepositoryCustom.java
+++ be/src/main/java/team4/codesquad/secondhand/repository/ProductRepositoryCustom.java
@@ -3,7 +3,8 @@ package team4.codesquad.secondhand.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import team4.codesquad.secondhand.domain.Product;
+import team4.codesquad.secondhand.service.dto.ProductSearchCondition;
 
 public interface ProductRepositoryCustom {
-    Slice<Product> findFilteredProducts(Pageable pageable);
+    Slice<Product> findFilteredProducts(Pageable pageable, ProductSearchCondition productSearchCondition);
 }
--- be/src/main/java/team4/codesquad/secondhand/repository/ProductRepositoryImpl.java
+++ be/src/main/java/team4/codesquad/secondhand/repository/ProductRepositoryImpl.java
@@ -1,11 +1,14 @@
 package team4.codesquad.secondhand.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import team4.codesquad.secondhand.constant.Status;
 import team4.codesquad.secondhand.domain.*;
+import team4.codesquad.secondhand.service.dto.ProductSearchCondition;
 
 import java.util.List;
 
@@ -20,11 +23,15 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Slice<Product> findFilteredProducts(Pageable pageable) {
+    public Slice<Product> findFilteredProducts(Pageable pageable, ProductSearchCondition productSearchCondition) {
         List<Product> products = queryFactory.selectFrom(product)
                 .leftJoin(product.location, location).fetchJoin()
                 .leftJoin(product.category, category).fetchJoin()
                 .leftJoin(product.user, user).fetchJoin()
+                .where(product.deleted.eq(false),
+                        locationEq(productSearchCondition.getLocationId()),
+                        categoryEq(productSearchCondition.getCategoryId()),
+                        saleStatusEq(productSearchCondition.getSaleStatus()))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize() + 1)
                 .fetch();
@@ -39,5 +46,26 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
         }
 
         return false;
+    }
+
+    private BooleanExpression locationEq(Integer locationId) {
+        if (locationId == null) {
+            return null;
+        }
+        return product.location.locationId.eq(locationId);
+    }
+
+    private BooleanExpression categoryEq(Integer categoryId) {
+        if (categoryId == null) {
+            return null;
+        }
+        return product.category.categoryId.eq(categoryId);
+    }
+
+    private BooleanExpression saleStatusEq(String saleStatus) {
+        if (saleStatus == null) {
+            return product.status.in(List.of(Status.SALE, Status.RESERVED));
+        }
+        return product.status.eq(Status.valueOf(saleStatus));
     }
 }
