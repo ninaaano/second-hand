ed2bc2db89a3e216018d0868750c018755a42538
Hyun <ghkdgus29@naver.com>
2023-06-21 14:27:44 +0900

    remove #68: JSON DTO 에서 불필요한 setter 제거

--- be/src/main/java/team4/codesquad/secondhand/service/dto/LocationDTO.java
+++ be/src/main/java/team4/codesquad/secondhand/service/dto/LocationDTO.java
@@ -3,11 +3,9 @@ package team4.codesquad.secondhand.service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import team4.codesquad.secondhand.domain.Location;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDTO {
