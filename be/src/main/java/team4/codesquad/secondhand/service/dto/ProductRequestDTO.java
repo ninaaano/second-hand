99db0c766c54789a2b4490bb304c46db3f5aa946
Life is a ninanino <tvxqinak@naver.com>
2023-06-23 11:16:52 +0900

    remove: 불필요한 어노테이션 삭제

--- be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
+++ be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
@@ -2,21 +2,15 @@ package team4.codesquad.secondhand.service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ProductRequestDTO {
-    @NotBlank(message = "제목을 입력해주세요")
     private String title;
     private Integer price;
-    @NotBlank(message = "내용을 입력해주세요")
     private String contents;
     private List<String> productImages;
     private int categoryId;
