137be397e6868ac796ac565de25a12d73d6f8fe9
Life is a ninanino <tvxqinak@naver.com>
2023-06-23 11:34:44 +0900

    feat: 요청되는 값이 없을 때 ExceptionHandler추가

--- be/src/main/java/team4/codesquad/secondhand/exception/ExceptionHandlers.java
+++ be/src/main/java/team4/codesquad/secondhand/exception/ExceptionHandlers.java
@@ -2,17 +2,27 @@ package team4.codesquad.secondhand.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import team4.codesquad.secondhand.controller.dto.Message;
+
 
 @ControllerAdvice
 public class ExceptionHandlers {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Message> handleNotFoundException(IllegalArgumentException e){
-        Message message = new Message(HttpStatus.BAD_REQUEST, e.getMessage(),null);
-        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+        Message message = new Message(HttpStatus.NOT_FOUND, e.getMessage(),null);
+        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Message> productRequestDTOValidationHandler(BindException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        Message message = new Message(HttpStatus.BAD_REQUEST, fieldError.getDefaultMessage(), null);
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
 }
--- be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
+++ be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class ProductRequestDTO {
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
     private Integer price;
+    @NotBlank(message = "내용을 입력해주세요")
     private String contents;
     private List<MultipartFile> productImages;
     private int categoryId;
