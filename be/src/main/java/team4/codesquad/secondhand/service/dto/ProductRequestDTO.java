1fad98fe6cea562c5019ad24b9a143e8bca81de1
Life is a ninanino <tvxqinak@naver.com>
2023-06-29 02:49:46 +0900

    feat: 이미지 등록 시 10장 초과하면 400 반환

--- be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
+++ be/src/main/java/team4/codesquad/secondhand/service/dto/ProductRequestDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
@@ -15,6 +16,7 @@ public class ProductRequestDTO {
     private Integer price;
     @NotBlank(message = "내용을 입력해주세요")
     private String contents;
+    @Size(min=1, max=10, message = "사진은 10장 이내로 등록 가능합니다.")
     private List<MultipartFile> productImages;
     private int categoryId;
     private int locationId;
