1fc7b898bb5c22d6a51d7aa2d9310dc4b781edef
sjuhan123 <sjuhan123@gmail.com>
2023-09-14 23:43:34 +0900

    refactor #128: auth page 로딩 로직 수정

--- fe/src/pages/Auth/index.tsx
+++ fe/src/pages/Auth/index.tsx
@@ -5,30 +5,31 @@ import { useUserLocationContext } from '@Contexts/userTownContext';
 import jwt_decode from 'jwt-decode';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {
+  USER_ALREADY_REGISTERED,
+  USER_SIGN_UP_IN_PROGRESS,
+} from '@Constants/auth';
 import { API_STATUS } from '@Constants/index';
 import { ROUTE_PATH } from '@Constants/route';
 import { User } from '@Types/index';
-import { USER_ALREADY_REGISTERED, USER_SIGN_UP_IN_PROGRESS } from './constant';
 import * as S from './style';
 
 const Auth = () => {
   const navigate = useNavigate();
   const { authInfo, authApiStatus, login } = useAuthContext();
   const { setUserInfo } = useUserContext();
-  const { status: locationApiStatus, fetchUserLocation } =
-    useUserLocationContext();
+  const { userLocationApiStatus, fetchUserLocation } = useUserLocationContext();
 
   useEffect(() => {
     login();
   }, []);
 
   useEffect(() => {
-    if (authInfo && authApiStatus === API_STATUS.SUCCESS) {
+    if (authInfo) {
       const JWTToken = authInfo.data;
       const { userId, username, avatar } = jwt_decode<User>(JWTToken);
 
       setUserInfo({ userId, username, avatar });
-
       if (authInfo.message === USER_SIGN_UP_IN_PROGRESS) {
         navigate(ROUTE_PATH.REGISTRATION);
       }
@@ -42,7 +43,7 @@ const Auth = () => {
 
   return (
     <S.Box>
-      {[authApiStatus, locationApiStatus].includes(API_STATUS.LOADING) && (
+      {[authApiStatus, userLocationApiStatus].includes(API_STATUS.LOADING) && (
         <Spinner isDynamic={false} />
       )}
     </S.Box>
